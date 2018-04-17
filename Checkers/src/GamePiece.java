2eb2e780e5ccb595076b9f5022209877131cfca5
323880625 <323880625@C-GQTXJQ1.tdsb.on.ca>
2018-04-17 14:36:02 -0400

    New methods for GamePieces
    You can now set a piece to king, get the colour of the piece outputted
    as a string, and check whether the piece is a king or not.

--- Checkers/src/GamePiece.java
+++ Checkers/src/GamePiece.java
@@ -1,13 +1,39 @@
 
 public class GamePiece {
 	
-	private Colour colour;
-	private boolean king;
+	private static Colour colour;
+	private static boolean king;
+	private static String colourString;
 	
 	
 	public GamePiece (Colour colour) {
 		
 		this.colour = colour;
+		king = false;
+		
+	}
+	
+	public static String getColour() {
+		
+		switch (colour) {
+		case RED: colourString = "r";
+		break;
+		case BLACK: colourString = "b";
+		}
+		
+		return colourString;
+		
+	}
+	
+	public static void makeKing() {
+		
+		king = true;
+		
+	}
+	
+	public static boolean checkKing() {
+		
+		return king;
 		
 	}
 	
