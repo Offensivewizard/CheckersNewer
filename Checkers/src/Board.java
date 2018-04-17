39d907f24118eee7e0faaaee398acff4b233c308
323863381 <323863381@C-2V5ZJM1.tdsb.on.ca>
2018-04-17 15:13:30 -0400

    worked on board constructor

--- Checkers/src/Board.java
+++ Checkers/src/Board.java
@@ -1,4 +1,42 @@
 
 public class Board {
 
+	GamePiece[][] board;
+
+	public Board() {
+
+		board = new GamePiece[8][8];
+		for (int i = 0; i < 8; i+=2) {
+
+			for (int t = 0; t < 8; t += 2) {
+				board[i][t].setColour(null);;
+			}
+		}
+		for (int i = 1; i < 4; i+=2) {
+
+			for (int t = 1; t < 8; t += 2) {
+				board[i][t].setColour(null);;
+			}
+		}
+		for (int i = 0; i < 2; i+=2) {
+
+			for (int t = 1; t < 8; t += 2) {
+				board[i][t].setColour(Colour.BLACK);;
+			}
+		}
+		
+		for(int i = 1; i<7; i+=2){
+			board[1][i].setColour(Colour.BLACK);;
+		}
+
+		for (int i = 0; i >8 ; i+=2) {
+			board[7][i].setColour(Colour.RED);
+		}
+		for (int i = 0; i >8 ; i+=2) {
+			board[5][i].setColour(Colour.RED);
+		}
+		for (int i = 1; i >7 ; i+=2) {
+			board[6][i].setColour(Colour.RED);
+		}
+	}
 }
