a86b8f0b869d30cc020ee25deb23cb1df3cbcd6e
323880625 <323880625@C-GQTXJQ1.tdsb.on.ca>
2018-04-19 13:10:33 -0400

    Again, tried to fix code.

--- Checkers/src/Board.java
+++ Checkers/src/Board.java
@@ -14,17 +14,24 @@ public class Board {
 				board[i][t] = new GamePiece(Colour.EMPTY);
 			}
 		}
-		for (int t = 1; t < 8; t += 2) {
-			board[1][t] = new GamePiece(Colour.EMPTY);
+		for (int i = 1; i < 8; i += 2) {
+			board[1][i] = new GamePiece(Colour.EMPTY);
 		}
 		for (int i = 3; i < 5; i++) {
 
 			for (int j = 0; j < 8; j++) {
 
 				board[i][j] = new GamePiece(Colour.EMPTY);
-
 			}
-
+		}
+		for (int i = 5 ; i < 8 ; i += 2) {
+			
+			for (int j = 1 ; j < 8 ; j += 2) {
+				board[i][j] = new GamePiece(Colour.EMPTY);
+			}
+		}
+		for (int i = 0 ; i < 8 ; i += 2 ) {
+			board[6][i] = new GamePiece(Colour.EMPTY);
 		}
 		for (int i = 0; i < 3; i += 2) {
 			for (int t = 1; t < 8; t += 2) {
@@ -47,7 +54,6 @@ public class Board {
 	}
 
 	public void display() {
-
 		for (int j = 0; j < 8; j++) {
 			for (int g = 0; g < 8; g++) {
 				System.out.print(board[j][g].getColour() + " ");
