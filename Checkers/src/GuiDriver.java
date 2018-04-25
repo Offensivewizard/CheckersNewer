201231ff631dc01773209f830eef2afe9f3541bb
EricWoo <eric.woo321@gmail.com>
2018-04-25 14:11:32 -0400

    Added the basis for GUI buttons that take co-ordinates

--- Checkers/src/GuiDriver.java
+++ Checkers/src/GuiDriver.java
@@ -1,15 +1,21 @@
 
 
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.control.ToggleButton;
+
 
 
 public class GuiDriver extends Application {
-	
-	Button[][] buttons = new Button[8][8];
+	private static final double BUTTON_WIDTH = 10;
+	private static final double BUTTON_HEIGHT = 10;
+	private static coordButton[][] buttons = new coordButton[8][8];
 	
 	public static void main(String[] args) {
 		
@@ -21,23 +27,38 @@ public class GuiDriver extends Application {
 	public void start(Stage window) throws Exception {
 		
 		 GridPane gridpane = new GridPane();
-		 populateButtons(gridpane);
+		 
+		 for (int row=0; row<8; row++){
+				for (int col=0; col<8; col++){
+					buttons[row][col] = new coordButton(row,col);
+					buttons[row][col].setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+					buttons[row][col].setStyle("-fx-base: #f44e42;");
+					buttons[row][col].setOnAction(new EventHandler<ActionEvent>() {
+			            @Override
+			            public void handle(ActionEvent event) {
+			            	System.out.println(((coordButton)event.getSource()).getRow());
+			            	System.out.println(((coordButton)event.getSource()).getCol());
+			            }
+			        });
+					
+					gridpane.add(buttons[row][col], col, row);
+				}
+				}
+		 
+		 
 		 Scene scene = new Scene(gridpane, 500, 500);
 		 window.setTitle("Checkers");
 	        window.setScene(scene);
 	        window.show();
 	}
 	
-	private void populateButtons(GridPane gp){
-		
-		for (int row=0; row<8; row++){
-			for (int col=0; col<8; col++){
-				buttons[col][row] = new Button("E");
-				gp.add(buttons[col][row], col, row);
-			}
-		}
-		
-	}
+	
+
+	
+	
+	
+	
+	
 }
 	
 	
