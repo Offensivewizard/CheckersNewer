74144cbef693522eb06644c4d682b38c45538048
323863381 <323863381@C-2V5ZJM1.tdsb.on.ca>
2018-04-25 14:11:11 -0400

    Worked on AI class

--- Checkers/src/AI.java
+++ Checkers/src/AI.java
@@ -1,14 +1,39 @@
+import java.util.Random;
 
 public class AI {
 
-public AI(GamePiece[][] board){
-	int length = board.length;
-	for(int i = 0; i<length; i++){
-		for(int t = 0; t<length; t++){
-			
-		}
+	public AI() {
+
+		
 	}
 	
-	
+	public int[] getAIMove() {
+		
+		int[] coords = new int[4];
+		Random rand = new Random();
+
+		int r = rand.nextInt(8) + 0;
+		
+		for(int i = 0; i<4; i++){
+			coords[i] = r;
+		}
+		
+		
+
+		return coords;
+
+
+		
+	}
 }
+
+/*while (!gameBoard.validateMove(coord[0], coord[1], coord[2], coord[3], currentTurn)) {
+gameBoard.display();
+System.out.println("Invalid coordinates, try again. Input your coordinates in order:");
+coord[0] = in.nextInt();
+coord[1] = in.nextInt();
+coord[2] = in.nextInt();
+coord[3] = in.nextInt();
+
 }
+*/
